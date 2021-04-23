4def0a3ddc17943fd2e9b8856b7f4f342e04f998
nabangi <alvaronabangi@gmail.com>
2021-04-23 22:15:49 +0300

    products added to cart with a toast message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+           Welcome to Liqsol
+        </p>
+        <p class="subtitle">
+           The best Apparel Store online
+        </p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest Products</h2>
+      </div>
+
+    <div class="column is-3" v-for="product in latestProducts" v-bind:key="product.id" v-bind:product="product">
+
+      <div class="box">
+        <figure class="image mb-4">
+          <img :src="product.get_thumbnail">
+        </figure>
+
+        <h3 class="is-size-4">{{product.name}}</h3>
+        <p class="is-size-6 has-text-grey">${{product.price}}</p>
+
+        <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
+      </div>
+     </div>
+   </div> 
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      latestProducts: []
+    }
+  },
   components: {
-    HelloWorld
+  },
+  mounted() {
+    this.getLatestProducts()
+  },
+  methods: {
+    async getLatestProducts() {
+      this.$store.commit('setIsLoading', true)
+
+      await axios
+       .get('/api/v1/latest-products/')
+       .then(response => {
+         this.latestProducts = response.data
+       })
+       .catch(error => {
+         console.log(error)
+       })
+
+      this.$store.commit('setIsLoading', false)
+    }
   }
 }
 </script>
+
+<style scoped>
+ .image {
+   margin-top: -1.25rem;
+   margin-left: -1.25rem;
+   margin-right: -1.25rem;
+ }
+</style>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -6,15 +6,15 @@
                     <img v-bind:src="product.get_image">
                 </figure>
 
-                <h1 class="title">{{product.name}}</h1>
+                <h1 class="title">{{ product.name }}</h1>
 
-                <p>{{product.description}}</p>
+                <p>{{ product.description }}</p>
             </div>
 
             <div class="column is-3">
                 <h2 class="subtitle">Information</h2>
 
-                <p><strong>Price: </strong>${{product.price}}</p>
+                <p><strong>Price: </strong>${{ product.price }}</p>
 
                 <div class="field has-addons mt-6">
                     <div class="control">
@@ -22,7 +22,7 @@
                     </div>
 
                     <div class="control">
-                        <a class="button is-dark">Add to cart</a>
+                        <a class="button is-dark" @click="addToCart()">Add to cart</a>
                     </div>
                 </div>
             </div>
@@ -32,6 +32,7 @@
 
 <script>
 import axios from 'axios'
+import { toast } from 'bulma-toast'
 
 export default {
     name: 'Product',
@@ -45,18 +46,45 @@ export default {
         this.getProduct()
     },
     methods: {
-        getProduct() {
+        async getProduct() {
+            this.$store.commit('setIsLoading', true)
+            
             const category_slug = this.$route.params.category_slug
             const product_slug = this.$route.params.product_slug
 
-            axios
+            await axios
                 .get('/api/v1/products/${category_slug}/${product_slug}')
                 .then(response => {
                     this.product = response.data
+
+                    document.title = this.product.name + ' | Liqsol'
                 })
                 .catch(error => {
                     console.log(error)
                 })
+            
+            this.$store.commit('setIsLoading', false)
+        },
+        addToCart() {
+            if (isNaN(  this.quantity) || this.quantity < 1) {
+                this.quantity = 1
+            }
+
+            const item = {
+                product: this.product,
+                quantity: this.quantity
+            }
+
+            this.$store.commit('addToCart', item)
+
+            toast({
+                message: 'The product was added to cart',
+                type: 'is-success',
+                dismissible: true,
+                pauseOnHover: true,
+                duration: 2000,
+                position: 'bottom-right',
+            })
         }
     }
 }
